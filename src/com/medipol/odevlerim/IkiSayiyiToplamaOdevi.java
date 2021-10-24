e1916e25786a482bc4ff357d5d9cba6f97bd6dd6
Ömer Olcay <omer_olcay25@hotmail.com>
2021-10-24 15:30:48 +0300

    Initial commit

---
+++ src/com/medipol/odevlerim/IkiSayiyiToplamaOdevi.java
@@ -0,0 +1,15 @@
+package com.medipol.odevlerim;
+
+public class IkiSayiyiToplamaOdevi {
+
+	public static void main(String[] args) {
+		
+		//sayi 1 e 2 yi atadım...
+		int a = 2;
+		
+		int b = 4;
+		
+		System.out.println(a+b);
+	}
+
+}
---
+++ src/com/medipol/operators/AritmetikOperator.java
@@ -0,0 +1,10 @@
+package com.medipol.operators;
+
+public class AritmetikOperator {
+
+	public static void main(String[] args) {
+		int a = 10;
+		System.out.println("Hello Java "+a);
+	}
+
+}
